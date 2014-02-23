ca67018fb3bbca9e2db2f6e05e452f55ab4e9d3b
Dissanayake <ooopsamila@gmail.com>
2014-02-23 17:45:51 +0530

    keeping backup

--- src/main/java/uok/mf/results/AddResultsForm.java
+++
@@ -1,7 +0,0 @@
-package uok.mf.results;
-
-/**
- * Created by amila on 2/22/14.
- */
-public class AddResultsForm {
-}
--- src/main/java/uok/mf/students/ManageStudentsForm.java
+++
@@ -1,7 +0,0 @@
-package uok.mf.students;
-
-/**
- * Created by amila on 2/22/14.
- */
-public class ManageStudentsForm {
-}
