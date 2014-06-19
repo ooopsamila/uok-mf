e9f69e1365ec7539e8adfd6d6427163ea1558c78
Amila Dissanayake <amilad@hsenidmobile.com>
2014-06-19 17:31:24 +0530

    added function to read excel files

--- src/main/java/uok/mf/service/StudentsService.java
+++ src/main/java/uok/mf/service/StudentsService.java
@@ -10,11 +10,18 @@ import uok.mf.repositories.StudentsRepository;
 import uok.mf.students.add.AddStudentForm;
 import uok.mf.students.manage.ManageStudentsForm;
 
+import java.io.File;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import jxl.Cell;
+import jxl.CellType;
+import jxl.Sheet;
+import jxl.Workbook;
+import jxl.read.biff.BiffException;
 
 /**
  * Created by amila on 2/22/14.
@@ -110,5 +117,35 @@ public class StudentsService {
         model.put("reportColumn", studentReport.getColHeaders());
         model.put("reportBeans", studentReport.getData());
         return model;
+    }
+    public void read() throws IOException {
+        File inputWorkbook = new File(inputFile);
+        Workbook w;
+        try {
+            w = Workbook.getWorkbook(inputWorkbook);
+            // Get the first sheet
+            Sheet sheet = w.getSheet(0);
+            // Loop over first 10 column and lines
+
+            for (int j = 0; j < sheet.getColumns(); j++) {
+                for (int i = 1; i < sheet.getRows(); i++) {
+
+                    Cell cell = sheet.getCell(map.get("studentNumber"), i);
+                    CellType type = cell.getType();
+                    if (type == CellType.LABEL) {
+                        System.out.println("I got a label "
+                                + cell.getContents());
+                    }
+
+                    if (type == CellType.NUMBER) {
+                        System.out.println("I got a number "
+                                + cell.getContents());
+                    }
+
+                }
+            }
+        } catch (BiffException e) {
+            e.printStackTrace();
+        }
     }
 }
